d8ade3096f460e7ab01652eadc1d4b55dd9c6811
Flow <guanjun211@gmail.com>
2024-10-17 14:12:20 +0800

    remove loading

--- js/loading.js
+++ js/loading.js
@@ -1,39 +1,39 @@
-$(window).on('load', function() {
-    // Hide loader after content is fully loaded
-    $('#loader').fadeOut('slow', function() {
-      $(this).addClass('hidden');
-    });
+// $(window).on('load', function() {
+//     // Hide loader after content is fully loaded
+//     $('#loader').fadeOut('slow', function() {
+//       $(this).addClass('hidden');
+//     });
   
-    // 原有的 Portfolio 初始化邏輯
-    var $container = $('.portfolioContainer'),
-        $body = $('body'),
-        colW = 620,
-        columns = null;
+//     // 原有的 Portfolio 初始化邏輯
+//     var $container = $('.portfolioContainer'),
+//         $body = $('body'),
+//         colW = 620,
+//         columns = null;
   
-    $container.isotope({
-      resizable: true,
-      masonry: {
-        columnWidth: colW
-      }
-    });
+//     $container.isotope({
+//       resizable: true,
+//       masonry: {
+//         columnWidth: colW
+//       }
+//     });
     
-    // Window resize handler to adjust layout
-    $(window).smartresize(function(){
-      var currentColumns = Math.floor(($body.width() - 30) / colW);
-      if (currentColumns !== columns) {
-        columns = currentColumns;
-        $container.width(columns * colW).isotope('reLayout');
-      }
-    }).smartresize();
+//     // Window resize handler to adjust layout
+//     $(window).smartresize(function(){
+//       var currentColumns = Math.floor(($body.width() - 30) / colW);
+//       if (currentColumns !== columns) {
+//         columns = currentColumns;
+//         $container.width(columns * colW).isotope('reLayout');
+//       }
+//     }).smartresize();
     
-    $('.portfolioFilter a').click(function(){
-      $('.portfolioFilter .current').removeClass('current');
-      $(this).addClass('current');
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        filter: selector,
-      });
-      return false;
-    });
-  });
+//     $('.portfolioFilter a').click(function(){
+//       $('.portfolioFilter .current').removeClass('current');
+//       $(this).addClass('current');
+//       var selector = $(this).attr('data-filter');
+//       $container.isotope({
+//         filter: selector,
+//       });
+//       return false;
+//     });
+//   });
   
